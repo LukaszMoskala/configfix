e727b7e260eda6619f5220c50c4c0ec81021935b
Łukasz Moskała <lm@lukaszmoskala.pl>
2019-06-15 23:32:51 +0200

    Put 'static' in version name

--- version.hpp
+++ version.hpp
@@ -1,5 +1,5 @@
 //version can be overridden using make compile flags
 //like this: make 'CXXFLAGS=-DVERSION=\"123.45-name\"'
 #ifndef VERSION
-#define VERSION "1.1"
+#define VERSION "1.1-static"
 #endif
